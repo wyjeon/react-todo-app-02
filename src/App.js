92537d7e6dce0f89327c3e94b1bafc05ea00dde4
wyjeon <wyjeon.info@gmail.com>
2021-02-26 12:04:55 +0900

    App에서 todos 상태 사용하기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링해보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ]);
   return (
     <div>
       <TodoTemplate>
         <TodoInsert />
-        <TodoList />
+        <TodoList todos={todos} />
       </TodoTemplate>
     </div>
   );
